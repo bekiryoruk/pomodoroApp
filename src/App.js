02bf94523872254df6134199dd8076836ffc15b2
Bekir Yörük <bekiryoruk1@gmail.com>
2021-03-26 15:35:21 +0300

    app is now totally working

--- src/App.js
+++ src/App.js
@@ -6,46 +6,83 @@ class App extends React.Component {
     this.state = {
       minutes: 30,
       break: 5,
-      total_time: 0,
+      total_time: 1800,
+      work: false,
     }
     this.decreaseMinutes = this.decreaseMinutes.bind(this)
+    this.increaseMinutes = this.increaseMinutes.bind(this)
+    this.decreaseBreak = this.decreaseBreak.bind(this)
+    this.increaseBreak = this.increaseBreak.bind(this)
+    this.timer = this.timer.bind(this)
   }
-  inputHandler = (e) => {
-    this.setState({
-      ...this.state,
-      [e.target.name]: e.target.value,
-    })
+  timer() {
+    if (this.state.work) {
+      this.setState({
+        total_time: this.state.total_time - 1,
+      })
+      if (this.state.total_time < 1) {
+        clearInterval(this.intervalId)
+      }
+    }
   }
-
-  startTime = () => {}
-  stopTime = () => {}
+  componentDidMount() {
+    this.intervalId = setInterval(this.timer.bind(this), 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+  startTime = () => {
+    this.setState((prevState) => ({
+      total_time: prevState.minutes * 60,
+      work: true,
+    }))
+  }
+  stopTime = () => {
+    this.setState((prevState) => ({
+      work: false,
+    }))
+  }
   resetTime = () => {
-    this.setState({ minutes: '', break: '' })
+    this.setState({ minutes: 0, break: 0 })
   }
   decreaseMinutes() {
     this.setState((prevState) => ({
       minutes: prevState.minutes - 1,
     }))
   }
-
+  increaseMinutes() {
+    this.setState((prevState) => ({
+      minutes: prevState.minutes + 1,
+    }))
+  }
+  decreaseBreak() {
+    this.setState((prevState) => ({
+      break: prevState.break - 1,
+    }))
+  }
+  increaseBreak() {
+    this.setState((prevState) => ({
+      break: prevState.break + 1,
+    }))
+  }
   render() {
     return (
       <div>
-        <h1>Welcome to Pomodoro</h1>
+        <div>
+          <h1>Welcome to Pomodoro</h1>
+        </div>
+        <div>
+          <button onClick={this.decreaseBreak}>-</button>
+          <h3>{this.state.break}</h3>
+          <button onClick={this.increaseBreak}>+</button>
+          <h3>break</h3>
+          <br />
+        </div>
         <div>
           <button onClick={this.decreaseMinutes}>-</button>
           <h3>{this.state.minutes}</h3>
           <button onClick={this.increaseMinutes}>+</button>
-          <h3>minutes</h3>
-
-          <button onClick={this.decreaseBreak}>-</button>
-
-          <h3>{this.state.break}</h3>
-          <button onClick={this.increaseBreak}>+</button>
-
-          <h3>break</h3>
-
-          <br />
+          <h3>working</h3>
         </div>
         <div>
           <button onClick={this.startTime}>Start</button>
@@ -53,7 +90,10 @@ class App extends React.Component {
           <button onClick={this.resetTime}>Reset</button>
         </div>
         <div>
-          <h1>The time left is: {this.state.break}</h1>
+          <h1>
+            The time left is: {Math.floor(this.state.total_time / 60)}:
+            {this.state.total_time % 60}
+          </h1>
         </div>
       </div>
     )
